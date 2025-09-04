f68da369f30c5b2a60d594f89c213ecc4bad0d0f
subhamnavratan <subhamkumarnavratan@gmail.com>
2025-09-04 23:02:21 +0530

    Fixed mongoDB  version

--- RESUME/src/main/java/com/Resume/RESUME/ResumeApplication.java
+++ RESUME/src/main/java/com/Resume/RESUME/ResumeApplication.java
@@ -2,12 +2,24 @@ package com.Resume.RESUME;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class ResumeApplication {
+
+	@Autowired
+	private MongoTemplate mongoTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ResumeApplication.class, args);
 	}
 
+	@PostConstruct
+	public void testConnection() {
+		System.out.println("MongoDB connected. Collections:");
+		mongoTemplate.getDb().listCollectionNames().forEach(System.out::println);
+	}
 }
+
