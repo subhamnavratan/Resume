fbbb62a78aa26240cd0020467bc34e0ac55aa711
subhamnavratan <subhamkumarnavratan@gmail.com>
2025-09-04 23:49:35 +0530

    Fixed frontend js

--- RESUME/src/main/resources/static/script.js
+++ RESUME/src/main/resources/static/script.js
@@ -1,4 +1,4 @@
-const baseURL = "https://resume-14.onrender.com"; // Replace with your backend URL
+const baseURL = "https://REsume.onrender.com"; // Replace with your backend URL
 const resumeId = "68b73987f247c44ed5dcdb8c"; // Example resume ID
 const mainContent = document.getElementById("mainContent");
 
